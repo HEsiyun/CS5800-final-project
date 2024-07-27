bac4aa5f3413271ff0e35655cd906c8cc580a7e3
Siyun He <hesiyun0603@gmail.com>
2024-07-27 11:35:05 -0700

    reivse the file org, rename insert.py to b_tree.py. Finished development of insert funciton of b_tree. revsise the insert_driver add more function. Currently it supports user to read in an excel dataframe to store it as b tree. The b tree can be printed and visualized

--- insert_driver.py
+++ insert_driver.py
@@ -1,33 +1,43 @@
 from read_data import read_in_data, choose_index, sort_data, add_row_number
-from insert import BTree, BTreeNode, choose_max_degree
+from b_tree import BTree, BTreeNode, choose_max_degree
 import pandas as pd
 
-def main():
-    data = read_in_data()
-    # #print data type of each column
-    # print(data.dtypes)
-    # print(data['student_ID'].dtype)
-    key = choose_index(data)
-    data = sort_data(data, key)
-    data = add_row_number(data)
-
-    # store row_number and key in a list of tuples, put key in the first element of the tuple
-    keys = list(zip(data[key], data['row_number']))
-    print(keys)
-    # get the max degree of the BTree
-    t = choose_max_degree(data.shape[0])
     
-    #create a BTree object
-    btree = BTree(t)
-    # insert the keys into the BTree
-    for key in keys:
-        btree.insertion(key)
-        # print the tree
-        btree.print_tree(btree.root)
-        print('-'*50)
-    # visualize the tree
-    btree.visualize()
 
 
-if __name__ == "__main__":
-    main()
+def insert_driver():
+    # ask the user whether to read in the data from a file or generate the data
+    while True:
+        try:
+            choice = int(input("Enter 1 to read in data from a file, 2 to generate data: "))
+            if choice not in [1, 2]:
+                raise ValueError("Invalid choice")
+            break
+        except ValueError as error:
+            print("Invalid value:", error)
+    if choice == 1:
+        data = read_in_data()
+        key = choose_index(data)
+        data = sort_data(data, key)
+        data = add_row_number(data)
+
+        # store row_number and key in a list of tuples, put key in the first element of the tuple
+        keys = list(zip(data[key], data['row_number']))
+        print(keys)
+        # get the max degree of the BTree
+        t = choose_max_degree(data.shape[0])
+        
+        #create a BTree object
+        btree = BTree(t)
+        # insert the keys into the BTree
+        for key in keys:
+            btree.insertion(key)
+            # print the tree
+            btree.print_tree(btree.root)
+            print('-'*50)
+        # visualize the tree
+        btree.visualize()
+
+
+insert_driver()
+
